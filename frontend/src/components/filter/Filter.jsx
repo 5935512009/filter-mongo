0f4e335b4971bb6413f46a3b699a991bd72dca18
5935512009 <Pasut2541@hotmail.com>
2024-09-19 15:12:10 +0700

    filter:{update css}

--- frontend/src/components/filter/Filter.jsx
+++ frontend/src/components/filter/Filter.jsx
@@ -1,40 +1,46 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './filter.css';
+
 function Filter() {
-  const [items,setItems] = useState([]);
-  const [search,setSearch] = useState('');
-  useEffect(()=>{
+  const [items, setItems] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
     axios
-      .get("http://localhost:4001/api/api/blog")
-      .then((res)=>{
+      .get('http://localhost:4001/api/api/blog')
+      .then((res) => {
         setItems(res.data);
-       // console.log(res.data);
       })
-      .catch((error)=>{
-        console.log("this error is" , error);
-      })
-  },[]);
-  console.log(search)
+      .catch((error) => {
+        console.log('this error is', error);
+      });
+  }, []);
+
   return (
-    <div>
+    <div className='filter'>
       <input
-        onChange={(e)=> setSearch(e.target.value)}
-        />
-        {items.filter((item)=>{
+        className='filter-input'
+        placeholder='Search for items...'
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {items
+        .filter((item) => {
           return search.toLowerCase() === ''
-          ?item:item.name.toLowerCase().includes(search);
-        }).map((item,index)=>(
-        <div key={index}>
-          <p>name  :{item.name}</p><br/>
-          <p>des   :{item.description}</p><br/>
-          <p>tools :{item.tools}</p><br/>
-          <p>master:{item.master}</p><br/>
-          ================================
-        </div>
-      ))}
-      
+            ? item
+            : item.name.toLowerCase().includes(search);
+        })
+        .map((item, index) => (
+          <div className='item' key={index}>
+            <p><strong>Name:</strong> {item.name}</p>
+            <p><strong>Description:</strong> {item.description}</p>
+            <p><strong>Tools:</strong> {item.tools}</p>
+            <p><strong>Master:</strong> {item.master}</p>
+            <hr className='divider' />
+          </div>
+        ))}
     </div>
-  )
+  );
 }
 
-export default Filter
+export default Filter;
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './navbar.css'
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <div className='navbar-menu'>
-        <ul>
+      <div className='nav-menu'>
+        <ul className='nav-list'>
           <li><Link to="./filter">Filter</Link></li>
         </ul>
       </div>
